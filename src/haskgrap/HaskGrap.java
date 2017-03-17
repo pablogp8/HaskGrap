18a25ee0b99e081a251be62c70dfbcc8340b4df6
Pablo Yeguech <pablo.ys@hotmail.com>
2017-03-17 03:33:07 -0600

    Salida de prueba 2

--- src/haskgrap/HaskGrap.java
+++ src/haskgrap/HaskGrap.java
@@ -16,6 +16,7 @@ public class HaskGrap {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.printf("hola mundo");
     }
     
 }
